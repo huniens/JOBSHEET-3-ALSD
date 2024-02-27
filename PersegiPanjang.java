ea02a5510afe4249bb78f1cc8ac2dd856abd59af
hunien <huniens06@gmail.com>
2024-02-27 16:03:52 +0700

    change

--- PersegiPanjang.java
+++ PersegiPanjang.java
@@ -1,34 +1,35 @@
 /**
- * PersegiPanjang
+ * InnerPersegiPanjang
  */
 public class PersegiPanjang {
     public int panjang;
     public int lebar;
+    public int Luas;
+    public int keliling;
 
-    // Konstruktor default
-    public PersegiPanjang() {
-        // Default constructor
-    }
-
-    public PersegiPanjang(int p, int l) {
-        panjang = p;
-        lebar = l;
+    // Konstruktor berparameter
+    public PersegiPanjang(int panjang, int lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        hitungLuas();
+        hitungKeliling();
     }
 
     // Method untuk menghitung luas
-    //public int hitungLuas() {
-        //return panjang * lebar;
-    //}
+    public void hitungLuas() {
+        Luas = panjang * lebar;
+    }
 
     // Method untuk menghitung keliling
-    //    return 2 * (panjang + lebar);
-    //}
-
+    public void hitungKeliling() {
+        keliling = 2 * (panjang + lebar);
+    }
+    
     // Method untuk mencetak informasi persegi panjang
     public void cetakInfo() {
         System.out.println("Panjang: " + panjang);
         System.out.println("Lebar: " + lebar);
-        //System.out.println("Luas: " + hitungLuas());
-        //System.out.println("Keliling: " + hitungKeliling());
+        System.out.println("Luas: " + Luas);
+        System.out.println("Keliling: " + keliling);
     }
 }
--- PersegiPanjangDemo.java
+++ PersegiPanjangDemo.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
+
 /**
  * PersegiPanjangDemo
  */
- public class PersegiPanjangDemo {
-
+public class PersegiPanjangDemo {
     public static void main(String[] args) {
         Scanner input13 = new Scanner(System.in);
-            PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[3];
-            int panjang, lebar;
-            
-            for (int i = 0; i < 3; i++) {
-                arrayOfPersegiPanjang[i] = new PersegiPanjang();
 
-                System.out.println("Persegi panjang ke-" + (i + 1));
-                System.out.print("Masukkan panjang: ");
-                panjang = input13.nextInt();
-                System.out.print("Masukkan lebar: ");
-                lebar = input13.nextInt();
+        System.out.print("Masukkan jumlah persegi panjang: ");
+        int jumlahPersegi = input13.nextInt();
 
-                arrayOfPersegiPanjang[i] = new PersegiPanjang(panjang,lebar);
+        PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[jumlahPersegi];
+        int panjang, lebar;
 
-                //System.out.print("Masukkan lebar: ");
-                //arrayOfPersegiPanjang[i].lebar = input13.nextInt();
-            }
-        
-            System.out.println("Persegi panjang ke-1, panjang: " + arrayOfPersegiPanjang[0].panjang + ", lebar: " + arrayOfPersegiPanjang[0].lebar);
-            System.out.println("Persegi panjang ke-2, panjang: " + arrayOfPersegiPanjang[1].panjang + ", lebar: " + arrayOfPersegiPanjang[1].lebar);
-            System.out.println("Persegi panjang ke-3, panjang: " + arrayOfPersegiPanjang[2].panjang + ", lebar: " + arrayOfPersegiPanjang[2].lebar);
+        for (int i = 0; i < jumlahPersegi; i++) {
+            System.out.println("Persegi panjang ke-" + (i + 1));
+            System.out.print("Masukkan panjang: ");
+            panjang = input13.nextInt();
+            System.out.print("Masukkan lebar: ");
+            lebar = input13.nextInt();
+
+            arrayOfPersegiPanjang[i] = new PersegiPanjang(panjang, lebar);
         }
+
+        for (int i = 0; i < jumlahPersegi; i++) {
+            System.out.println("Informasi Persegi panjang ke-" + (i + 1) + ":");
+            arrayOfPersegiPanjang[i].cetakInfo();
+            System.out.println(); // Tambahkan baris kosong antar persegi panjang
+        }
+
+        input13.close(); // Tutup scanner setelah digunakan
+    }
 }
